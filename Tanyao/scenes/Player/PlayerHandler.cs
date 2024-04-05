ada220eb5440e4c5af736987ab1285a601887b6e
Roger! <mahjong.mahjong@gmail.com>
2024-04-05 11:35:11 -0700

    feat: add initial implementaiton of getting the score

--- Tanyao/scenes/Player/PlayerHandler.cs
+++ Tanyao/scenes/Player/PlayerHandler.cs
@@ -1,6 +1,6 @@
 using Godot;
 using System;
-
+using Mahjong;
 //TODO: not sure if inheritance is the right thing to do
 public partial class PlayerHandler : BaseHandler
 {	
@@ -9,6 +9,9 @@ public partial class PlayerHandler : BaseHandler
 	
 	[Export]
 	Label _ShantenLabel;
+	
+	[Export]
+	Label _WinLabel;
 	
 	//Events _Events;
 	
@@ -37,10 +40,10 @@ public partial class PlayerHandler : BaseHandler
 	public void AddTileToHandTsumo(Mahjong.Model.Tile poNewTileModel)
 	{
 		_PlayerHand.AddTileToHandTsumo(poNewTileModel);
-		IsValidHand();
+		IsValidHand(poNewTileModel);
 	}
 	
-	public bool IsValidHand()
+	public bool IsValidHand(Mahjong.Model.Tile poWinTile)
 	{
 		Mahjong.Model.Hand oHand = new Mahjong.Model.Hand();
 		foreach(TileUI oTileUI in _PlayerHand._HandClosed.GetChildren())
@@ -64,9 +67,55 @@ public partial class PlayerHandler : BaseHandler
 		if(nShanten == -1)
 		{
 			//TODO evaluate the type of hand here.
+			oHand.Agari = Enums.Agari.Tsumo;
+			oHand.SeatWind = Enums.Wind.East;
+			oHand.RoundWind = Enums.Wind.East;
+			oHand.WinTile = poWinTile;
+			
+			Mahjong.CScoreEvaluator oScoreEvaluator = new Mahjong.CScoreEvaluator();
+			Mahjong.Model.Score oScore = oScoreEvaluator.EvaluateScore(oHand);
+			string sWinLabelText = "";
+			
+			foreach(Enums.Yaku yaku in oScore.YakuList)
+			{
+				sWinLabelText += yaku + "\n";
+				GD.Print(yaku);
+			}
+			sWinLabelText += oScore.Han + " Han " + oScore.Fu + " Fu";
+			UpdateWinLabel(sWinLabelText);
 		}
 		
 		return false;
+	}
+	
+	public async void UpdateWinLabel(string psWinLabelText)
+	{
+		_WinLabel.Text = psWinLabelText;
+		await ToSignal(GetTree().CreateTimer(5), "timeout");	
+		_WinLabel.Text = "";
+	}
+	
+	
+	public void TestWinLabel()
+	{
+		Mahjong.CHandParser oHandParser = new Mahjong.CHandParser();
+		Mahjong.Model.Hand oHand = oHandParser.ParseHand("123456s234567p55m");
+		oHand.WinTile = new Mahjong.Model.Tile("7p");
+		oHand.Agari = Enums.Agari.Tsumo;
+		oHand.SeatWind = Enums.Wind.East;
+		oHand.RoundWind = Enums.Wind.East;
+		
+		Mahjong.CScoreEvaluator oScoreEvaluator = new Mahjong.CScoreEvaluator();
+		Mahjong.Model.Score oScore = oScoreEvaluator.EvaluateScore(oHand);
+		string sWinLabelText = "";
+		
+		foreach(Enums.Yaku yaku in oScore.YakuList)
+		{
+			sWinLabelText += yaku + "\n";
+			GD.Print(yaku);
+		}
+		sWinLabelText += oScore.Han + " Han " + oScore.Fu + " Fu";
+		_WinLabel.Text = sWinLabelText;
 	}
 	
 	public void OnPlayerTileDiscarded()
