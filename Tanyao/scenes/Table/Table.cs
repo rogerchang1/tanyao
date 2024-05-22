99a19396f35248b40958c83766cb0e5804add36e
Roger! <mahjong.mahjong@gmail.com>
2024-05-22 12:53:03 -0700

    feat: add wind switching functionality and limit the draw tiles to 36

--- Tanyao/scenes/Table/Table.cs
+++ Tanyao/scenes/Table/Table.cs
@@ -24,6 +24,11 @@ public partial class Table : Godot.Node2D
 	public int _EnemyPoints;
 	public int _Pot;
 	public int _Honba;
+	//For 2 players, round 1, 2 means East. Round 3, 4 means South.
+	public int _RoundNumber;
+	
+	public int _TileDrawLimit;
+	public int _TileDrawCounter;
 	
 	//TODO: Think about additional dora tiles when kan 
 	public Mahjong.Model.Tile _DoraIndicator;
@@ -58,11 +63,32 @@ public partial class Table : Godot.Node2D
 		_Events.PlayerWinDeclared += OnPlayerWinDeclared;
 		_Events.RiichiDeclared += OnRiichiDeclared;
 		
-		_PlayerHandler._PlayerPoints = 25000;
-		_EnemyPoints = 25000;
-		_Honba = 0;
+		
+		InitializeTableValues();
 		UpdateDebugInfoLabel();
 		InitializeTable();
+	}
+	
+	public void InitializeTableValues()
+	{
+		_PlayerHandler._PlayerPoints = 25000;
+		_EnemyPoints = 25000;
+		_RoundWind = Enums.Wind.East;
+		_RoundNumber = 1;
+		_Honba = 0;
+		_TileDrawLimit = 36;
+		//TODO: randomize who draws first
+		Random rnd = new Random();
+		int coinToss   = rnd.Next(2);
+		if(coinToss == 0)
+		{
+			_PlayerHandler._SeatWind = Enums.Wind.East;
+			_EnemyHandler._SeatWind = Enums.Wind.South;
+		}else{
+			_PlayerHandler._SeatWind = Enums.Wind.South;
+			_EnemyHandler._SeatWind = Enums.Wind.East;
+		}
+		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -72,25 +98,22 @@ public partial class Table : Godot.Node2D
 	
 	public void InitializeTable()
 	{
-		//_TableManager.InitializeTable(_TableModel);
 		Mahjong.Model.WallConfiguration oWallConfig = new Mahjong.Model.WallConfiguration();
 		oWallConfig.LoadManzu = false;
 		oWallConfig.LoadWest = false;
 		oWallConfig.LoadNorth = false;
 		//oWallConfig.LoadEast = false;
 		//oWallConfig.LoadSouth = false;
-		oWallConfig.LoadDragons = false;
-		oWallConfig.LoadPinzu = false;
+		//oWallConfig.LoadDragons = false;
+		//oWallConfig.LoadPinzu = false;
 		_TableManager.InitializeTableWithWallConfiguration(_TableModel,oWallConfig);
+		_TileDrawCounter = 0;
 		_DoraIndicator = _TableModel.Wall[_TableModel.Wall.Count - 5];
 		SetDora();
-		_RoundWind = Enums.Wind.East;
-		_PlayerHandler._SeatWind = Enums.Wind.East;
 		_PlayerHandler._DoraTile = _DoraTile;
 		_DoraIndicatorLabel.Text = "DoraIndicator: " + _DoraIndicator.ToString() + "\nDora: " + _DoraTile.ToString();
-		_RoundWindLabel.Text = "RoundWind: " + _RoundWind.ToString();
-		_SeatWindLabel.Text = "SeatWind: " + _PlayerHandler._SeatWind.ToString();
 		
+		//call tiles debug
 		//_TableModel.Wall[0] = new Mahjong.Model.Tile("6s");
 		//_TableModel.Wall[1] = new Mahjong.Model.Tile("2p");
 		//_TableModel.Wall[2] = new Mahjong.Model.Tile("3p");
@@ -127,11 +150,50 @@ public partial class Table : Godot.Node2D
 		//_TableModel.Wall[32] = new Mahjong.Model.Tile("8p");
 		//_TableModel.Wall[33] = new Mahjong.Model.Tile("8p");
 		
+		////chiitoi debug
+		//_TableModel.Wall[0] = new Mahjong.Model.Tile("1s");
+		//_TableModel.Wall[1] = new Mahjong.Model.Tile("1s");
+		//_TableModel.Wall[2] = new Mahjong.Model.Tile("4s");
+		//_TableModel.Wall[3] = new Mahjong.Model.Tile("4s");
+		//_TableModel.Wall[4] = new Mahjong.Model.Tile("6s");
+		//_TableModel.Wall[5] = new Mahjong.Model.Tile("2p");
+		//_TableModel.Wall[6] = new Mahjong.Model.Tile("3p");
+		//_TableModel.Wall[7] = new Mahjong.Model.Tile("4p");
+		//_TableModel.Wall[8] = new Mahjong.Model.Tile("8s");
+		//_TableModel.Wall[9] = new Mahjong.Model.Tile("8s");
+		//_TableModel.Wall[10] = new Mahjong.Model.Tile("9s");
+		//_TableModel.Wall[11] = new Mahjong.Model.Tile("9s");
+		//_TableModel.Wall[12] = new Mahjong.Model.Tile("5p");
+		//_TableModel.Wall[13] = new Mahjong.Model.Tile("6p");
+		//_TableModel.Wall[14] = new Mahjong.Model.Tile("7p");
+		//_TableModel.Wall[15] = new Mahjong.Model.Tile("8p");
+		//_TableModel.Wall[16] = new Mahjong.Model.Tile("1z");
+		//_TableModel.Wall[17] = new Mahjong.Model.Tile("1z");
+		//_TableModel.Wall[18] = new Mahjong.Model.Tile("2z");
+		//_TableModel.Wall[19] = new Mahjong.Model.Tile("2z");
+		//_TableModel.Wall[20] = new Mahjong.Model.Tile("2s");
+		//_TableModel.Wall[21] = new Mahjong.Model.Tile("3s");
+		//_TableModel.Wall[22] = new Mahjong.Model.Tile("4s");
+		//_TableModel.Wall[23] = new Mahjong.Model.Tile("5s");
+		//_TableModel.Wall[24] = new Mahjong.Model.Tile("2s");
+		//_TableModel.Wall[25] = new Mahjong.Model.Tile("2s");
+		//
+		//_TableModel.Wall[26] = new Mahjong.Model.Tile("3s");
+		//_TableModel.Wall[27] = new Mahjong.Model.Tile("3s");
+		//_TableModel.Wall[28] = new Mahjong.Model.Tile("2s");
+		//_TableModel.Wall[29] = new Mahjong.Model.Tile("2s");
+		//_TableModel.Wall[30] = new Mahjong.Model.Tile("4p");
+		//_TableModel.Wall[31] = new Mahjong.Model.Tile("4p");
+		//_TableModel.Wall[32] = new Mahjong.Model.Tile("8p");
+		//_TableModel.Wall[33] = new Mahjong.Model.Tile("8p");
 		
-		//TODO: randomize who draws first
-		_PlayerHandler._SeatWind = Enums.Wind.East;
-		_EnemyHandler._SeatWind = Enums.Wind.South;
-		InitializeHands(true);
+		bool bDealToPlayerFirst = true;
+		if(_PlayerHandler._SeatWind != Enums.Wind.East)
+		{
+			bDealToPlayerFirst = false;
+		}
+		InitializeHands(bDealToPlayerFirst);
+		UpdateDebugInfoLabel();
 		UpdateTilesLeftLabel();
 		StartRound();
 	}
@@ -202,15 +264,20 @@ public partial class Table : Godot.Node2D
 	
 	public void StartRound()
 	{
-		//TODO: Later when there's an enemy handler
-		//Just do a rand to flip a coin on who starts
-		
-		_PlayerHandler.StartTurn();
+		_PlayerHandler.SortTilesUI();
+		_EnemyHandler.SortTilesUI();
+		if(_PlayerHandler._SeatWind == Enums.Wind.East)
+		{
+			_PlayerHandler.StartTurn();
+		}
+		else{
+			_EnemyHandler.StartTurn();
+		}	
 	}
 	
 	public void OnDrawTileRequested(BaseHandler oBaseHandler)
 	{
-		if(!_TableManager.CanDrawFromWall(_TableModel))
+		if(!_TableManager.CanDrawFromWall(_TableModel) || _TileDrawCounter > _TileDrawLimit)
 		{
 			Ryuukyoku();
 			return;
@@ -226,6 +293,7 @@ public partial class Table : Godot.Node2D
 			//TODO: change this later
 			((EnemyHandler) oBaseHandler).AddTileToHandTsumo(DrawnTile);
 		}
+		_TileDrawCounter++;
 		UpdateTilesLeftLabel();
 	}
 	
@@ -242,9 +310,32 @@ public partial class Table : Godot.Node2D
 			_EnemyPoints += 1000;
 			_PlayerHandler._PlayerPoints -= 1000;
 		}
+		
+		if((_PlayerHandler._SeatWind == Enums.Wind.East && nPlayerShanten != 0)
+		|| (_EnemyHandler._SeatWind == Enums.Wind.East && nEnemyShanten != 0))
+		{
+			SwitchWinds();
+		}
 		_Honba += 1;
 		UpdateDebugInfoLabel();
 		OnRoundEnded();
+	}
+	
+	public void SwitchWinds()
+	{
+		if(_RoundWind == Enums.Wind.East && _RoundNumber == 2)
+		{
+			_RoundWind = Enums.Wind.South;
+		}
+		if(_PlayerHandler._SeatWind == Enums.Wind.East)
+		{
+			_PlayerHandler._SeatWind = Enums.Wind.South;
+			_EnemyHandler._SeatWind = Enums.Wind.East;
+		}else{
+			_PlayerHandler._SeatWind = Enums.Wind.East;
+			_EnemyHandler._SeatWind = Enums.Wind.South;
+		}
+		_RoundNumber++;
 	}
 	
 	public void OnPlayerTurnStarted(string psTile)
@@ -290,6 +381,7 @@ public partial class Table : Godot.Node2D
 		{
 			_Honba += 1;
 		}else{
+			SwitchWinds();
 			_Honba = 0;
 		}
 		
@@ -302,6 +394,8 @@ public partial class Table : Godot.Node2D
 		_PlayerPointsLabel.Text = "PlayerPoints: " + _PlayerHandler._PlayerPoints.ToString();
 		_PotLabel.Text = "Pot: " + _Pot.ToString();
 		_HonbaLabel.Text = "Honba: " + _Honba.ToString();
+		_RoundWindLabel.Text = "RoundWind: " + _RoundWind.ToString();
+		_SeatWindLabel.Text = "SeatWind: " + _PlayerHandler._SeatWind.ToString();
 	}
 	
 	public void OnRiichiDeclared()
@@ -312,6 +406,7 @@ public partial class Table : Godot.Node2D
 	
 	private void UpdateTilesLeftLabel()
 	{
-		 _TilesLeftLabel.Text = "Tiles Left: " + (_TableModel.Wall.Count - 14);
+		int count = Math.Min((_TileDrawLimit - _TileDrawCounter),(_TableModel.Wall.Count - 14)) ;
+		_TilesLeftLabel.Text = "Tiles Left: " + count;
 	}
 }
