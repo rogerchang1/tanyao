9028cca7a4c63bd7253bc25fd7fa53273c11b277
Roger! <mahjong.mahjong@gmail.com>
2024-05-24 12:09:43 -0700

    feat: update handscore scene with additional information

--- Tanyao/scenes/Table/HandScore.cs
+++ Tanyao/scenes/Table/HandScore.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 using Mahjong;
 
 public partial class HandScore : Node2D
@@ -12,17 +13,27 @@ public partial class HandScore : Node2D
 	
 	[Export]
 	public PackedScene _TileUIScene;
+	[Export]
+	public PackedScene _ChiScene;
+	[Export]
+	public PackedScene _PonScene;
 	
 	Label _WinLabel;
+	Label _AgariLabel;
 	
 	HBoxContainer _ClosedHand;
+	HBoxContainer _OpenHand;
+	HBoxContainer _WinTile;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		_Events = GetNode<Events>("/root/Events");
 		_ClosedHand = GetNode<HBoxContainer>("ClosedHand");
+		_OpenHand = GetNode<HBoxContainer>("OpenHand");
+		_WinTile = GetNode<HBoxContainer>("WinTile");
 		_WinLabel = GetNode<Label>("WinLabel");
+		_AgariLabel = GetNode<Label>("AgariLabel");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -57,13 +68,46 @@ public partial class HandScore : Node2D
 			
 			Mahjong.CTilesManager oTilesManager = new CTilesManager();
 			oTilesManager.SortTiles(_Hand.Tiles);
-			for(int i = 0; i < _Hand.Tiles.Count;i++)
+			
+			TileUI oWinTile = (TileUI) _TileUIScene.Instantiate();
+			_WinTile.AddChild(oWinTile);
+			oWinTile.SetTile(_Hand.WinTile);
+			
+			if(_Hand.Agari == Enums.Agari.Tsumo)
 			{
-				Mahjong.Model.Tile oTile = _Hand.Tiles[i];
+				_AgariLabel.Text = "TSUMO";
+			}else{
+				_AgariLabel.Text = "RON";
+			}
+			
+			oTilesManager.RemoveSingleTileOf(_Hand.Tiles,_Hand.WinTile);
+			
+			List<Mahjong.Model.Tile> ClosedTileList = oTilesManager.GetTileListWithBlocksRemoved(_Hand.Tiles,_Hand.LockedBlocks);
+			for(int i = 0; i < ClosedTileList.Count;i++)
+			{
+				Mahjong.Model.Tile oTile = ClosedTileList[i];
 				TileUI oTileUI = (TileUI) _TileUIScene.Instantiate();
 				_ClosedHand.AddChild(oTileUI);
 				oTileUI.SetTile(oTile);
 				oTileUI._IsInteractable = false;
+			}
+			for(int i = 0; i < _Hand.LockedBlocks.Count;i++)
+			{
+				Mahjong.Model.Block oBlock = _Hand.LockedBlocks[i];
+				if(oBlock.Type == Enums.Mentsu.Shuntsu)
+				{
+					LockedBlock ChiBlock = (LockedBlock) _ChiScene.Instantiate();
+					_OpenHand.AddChild(ChiBlock);
+					_OpenHand.MoveChild(ChiBlock, 0);
+					ChiBlock.SetUp(oBlock.Tiles[0], oBlock.Tiles[1], oBlock.Tiles[2]);
+				}
+				if(oBlock.Type == Enums.Mentsu.Koutsu)
+				{
+					LockedBlock PonBlock = (LockedBlock) _PonScene.Instantiate();
+					_OpenHand.AddChild(PonBlock);
+					_OpenHand.MoveChild(PonBlock, 0);
+					PonBlock.SetUp(oBlock.Tiles[0], oBlock.Tiles[1], oBlock.Tiles[2]);
+				}
 			}
 		}
 	}
